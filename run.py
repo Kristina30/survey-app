f964e5267f2ffa0708939779ed09600fb23bf17f
Kristina Borisova Hadzhieva <kristinahadzhieva111@gmail.com>
2021-09-06 19:48:15 +0000

    Add function to go through each question and wait for user input for the answers

--- run.py
+++ run.py
@@ -13,3 +13,30 @@ CREDS = Credentials.from_service_account_file('CREDS.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Survey')
+
+def get_survey_data():
+    """
+    This method gets data from the Survey spredsheet and display it to the user
+    """
+
+    survey_data = SHEET.worksheet("survey_data").get_all_values()
+
+    for survey_row in survey_data:
+        print(survey_row)
+
+def ask_survey_questions():
+    """
+    This method gets the questions from the sruvey spread sheet
+    """
+
+    survey_data = SHEET.worksheet("survey_data").get_all_values()
+    questions_row = survey_data[0]
+    for int in range(len(questions_row)):
+        print(questions_row[int] + "? ")
+        question_answer = input("Answer:\n ")
+    
+
+    
+
+#get_survey_data()
+ask_survey_questions()
